4d54c87116204ac4bd105acd167164e9f1f15568
SAURABH KUMAR <33839674+srvkmr130@users.noreply.github.com>
2017-12-08 08:32:40 +0530

    Add open in safe mode (issue)

--- background.js
+++ background.js
@@ -209,7 +209,7 @@ $(document).ready(function () {
           // chrome.tabs.create({ 'url': 'https://www.youtube.com/results?search_query=' + data.result.parameters.any });
                 } else if (data.result.metadata.intentName === "open") {
                    chrome.tabs.create({ 'url': "http://www." + data.result.parameters.website });
-                } else if (data.result.metadata.intentName === "incognito"||data.result.metadata.intentName === "safemode"||                   data.result.metadata.intentName === "privatebrowsing"||data.result.metadata.intentName === "openinsafemode") {
+                } else if (data.result.metadata.intentName === "incognito"||data.result.metadata.intentName === "safemode"||data.result.metadata.intentName === "privatebrowsing"||data.result.metadata.intentName === "openinsafemode") {
                    chrome.windows.create({url:"http://www.google.com",incognito: true});
                    Speech("Have   you   allowed  me  to   open   in   incognito  mode ?    If not   go  to chrome extension and give the permission");                   
                 }else if (data.result.metadata.intentName === "history") {
