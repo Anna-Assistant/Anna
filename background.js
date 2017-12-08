78e52d00cc6086ee5daeb946255b25b868041557
SAURABH KUMAR <33839674+srvkmr130@users.noreply.github.com>
2017-12-08 08:53:06 +0530

    indentation

--- background.js
+++ background.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
                    chrome.tabs.create({ 'url': 'chrome://downloads' });
                 } else if (data.result.metadata.intentName === "mail") {
                    chrome.tabs.create({ 'url': "https://mail.google.com/mail/?view=cm&fs=1&body=" + data.result.parameters.any });
-	              } else if (data.result.metadata.intentName === "tweet") {
+	        } else if (data.result.metadata.intentName === "tweet") {
                    tweet(data.result.parameters.any);
                 // chrome.tabs.create({ 'url': "http://www." + data.result.parameters.website });
                 } else if (data.result.metadata.intentName === "maps") {
